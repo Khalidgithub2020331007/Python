f1355581f5ec692808d45bb647f545bf3ea87c26
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-06-14 00:11:09 +0600

    Create floating-point precision error .py

---
+++ floating-point precision error .py
@@ -0,0 +1,10 @@
+a = 900000000000000694
+b = 6
+
+# Ceil using integer arithmetic
+ceil_result = (a + b - 1) // b
+print("Ceil result using integer arithmetic:", ceil_result)
+
+# Floor using integer arithmetic
+floor_result = a // b
+print("Floor result using integer arithmetic:", floor_result)
