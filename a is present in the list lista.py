ba70bcab4afef2bcb517ba6f5fdcc61db320b2d4
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-06-14 20:23:42 +0600

    Create a is present in the list lista.py

---
+++ a is present in the list lista.py
@@ -0,0 +1,15 @@
+lista = [1, 2, 3, 4, 5]
+
+# Check if an element is in the list
+a = 3
+if a in lista:
+    print(f"{a} is in the list")
+else:
+    print(f"{a} is not in the list")
+
+# Check if an element is not in the list
+b = 6
+if b not in lista:
+    print(f"{b} is not in the list")
+else:
+    print(f"{b} is in the list")
