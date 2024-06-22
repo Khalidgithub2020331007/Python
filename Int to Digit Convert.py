14d549579980511a9bb5db52cf6569adb062300d
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-06-22 21:43:16 +0600

    Update Int to Digit Convert.py

--- Int to Digit Convert.py
+++ Int to Digit Convert.py
@@ -1,5 +1,16 @@
+# 1.........
 def int_to_digits(n):
     return list(map(int, str(n)))
+
+# Example usage
+number = 12345
+digits = int_to_digits(number)
+print(digits)  # Output: [1, 2, 3, 4, 5]
+
+
+# 2......
+def int_to_digits(n):
+    return [int(digit) for digit in str(n)]
 
 # Example usage
 number = 12345
