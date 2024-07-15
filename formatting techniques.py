4efe88fdc47b387c826dce2db9dc6c314b3d7d17
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-07-15 09:53:57 +0600

    Create formatting techniques.py

---
+++ formatting techniques.py
@@ -0,0 +1,8 @@
+number = 3.14159
+formatted_number = "{:.2f}".format(number)
+print(formatted_number)  # Output: 3.14
+
+
+number = 3.14159
+formatted_number = f"{number:.2f}"
+print(formatted_number)  # Output: 3.14
