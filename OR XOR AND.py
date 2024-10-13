9af5d780fb899739bb42633508ecb620946870ea
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-10-13 19:41:18 +0600

    Create OR XOR AND.py

---
+++ OR XOR AND.py
@@ -0,0 +1,7 @@
+x = 6  # 110 in binary
+y = 3  # 011 in binary
+
+# Bitwise operations
+print(x & y)   # Output: 2 (010 in binary)
+print(x | y)   # Output: 7 (111 in binary)
+print(x ^ y)   # Output: 5 (101 in binary)
