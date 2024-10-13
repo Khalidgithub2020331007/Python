815f5bc0d92f8492f171adc623b088267a3fc40e
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-10-13 17:26:37 +0600

    Create Lower and Upper.py

---
+++ Lower and Upper.py
@@ -0,0 +1,11 @@
+s = "Hello, World!"
+upper_s = s.upper()  # Converts all characters to uppercase
+print(upper_s)  # Output: "HELLO, WORLD!"
+
+text = "Python is Fun!"
+
+# Convert to lowercase
+print(text.lower())  # Output: "python is fun!"
+
+# Convert to uppercase
+print(text.upper())  # Output: "PYTHON IS FUN!"
