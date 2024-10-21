4b571b0aa7ba6a16b4c587645d3c9a8042a055a1
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-10-21 08:26:00 +0600

    Create Ready.py

---
+++ Ready.py
@@ -0,0 +1,13 @@
+# 
+# def solve():
+# 
+# for _ in range(int(input())):
+#     solve()
+# solve()
+# 
+# for i in range(n):
+# for i in arr:
+# n=int(input())
+# s=input()
+# arr=list(map(int,input().split()))
+# n,m=map(int,input().split())
