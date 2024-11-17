3738d6314af5a94855079a28eb7a38cb8e1125b1
Sheikh Khalid Azad <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-11-17 14:59:09 +0600

    Update Lexicographically smallest string can obtain by deleting a single character from  S .py

--- Lexicographically smallest string can obtain by deleting a single character from  S .py
+++ Lexicographically smallest string can obtain by deleting a single character from  S .py
@@ -5,9 +5,13 @@ def solve():
     input()
     a=input()
     x=a+a
-    for i in range(len(a)):
-        x=min(x,a[:i]+a[i+1:])
-    print(x)
+    for i in range(len(a)-1):
+        if a[i]>a[i+1]:
+            print(a[:i]+a[i+1:])
+            return
+            break
+    print( a[:len(a)-1])
+
 
 
 
